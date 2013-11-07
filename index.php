99a3dc5c9226207fed2facfd097ad4749dcac9ef
mnakalay <mnakalay@yahoo.fr>
2013-11-07 09:37:13 +0000

    Fixed several bugs
    1- Files and folders were not properly ordered, used natcasesort to take into account upper and lower case in names.
    2- Folder with dots in the names were presented as files. It's fixed.
    3- Folders or files starting with dots were not showing. It's fixed.
    4- Replaced a for to use foreach instead to simplify code.

--- index.php
+++ index.php
@@ -1,73 +1,72 @@
 <!DOCTYPE html>
 <html>
 <head>
-   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-   <link rel="stylesheet" href="style.css">
-   <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,300' rel='stylesheet' type='text/css'>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,300' rel='stylesheet' type='text/css'>
 
-   <title>Localhost</title>
-   <style>
-     *{
-         padding:0;
-         margin:0;
-     }
+	<title>Localhost</title>
+	<style>
+	*{
+		padding:0;
+		margin:0;
+	}
 
 
-   </style>
+	</style>
 
 </head>
 <body>
 
-   <div id="container">
-       <?php
-         // opens this directory
-         $myDirectory = opendir(".");
+	<div id="container">
+		<?php
+				 // opens this directory
+		$myDirectory = opendir(".");
 
-         // gets each entry
-         while($entryName = readdir($myDirectory)) {
-           $dirArray[] = $entryName;
-         }
+				 // gets each entry
+		while($entryName = readdir($myDirectory)) {
+			$dirArray[] = $entryName;
+		}
 
-         // finds extention of file
-         function findexts ($filename)
-         {
-           $filename = strtolower($filename) ;
-           $exts = split("[/\\.]", $filename) ;
-           $n = count($exts)-1;
-           $exts = $exts[$n];
-           return strtoupper($exts);
-         }
+				 // finds extention of file
+		function findexts ($filename)
+		{
+			$filename = strtolower($filename) ;
+			$exts = split("[/\\.]", $filename) ;
+			$n = count($exts)-1;
+			$exts = $exts[$n];
+			return strtoupper($exts);
+		}
 
-         // closes directory
-         closedir($myDirectory);
+				 // closes directory
+		closedir($myDirectory);
+				 // sorts files
+		if(count($dirArray)) {
+			natcasesort($dirArray);
+		}
 
-         //  counts elements in array
-         $indexCount   = count($dirArray);
 
-         // sorts files
-         sort($dirArray);
+				 // print 'em
+		print("<h1>Localhost Directory</h1>");
+		print("<table cellspacing='0' class='table'>
+			<tr>
+			<td class='head'>Filename</td>
+			<td class='head'>Type</td>
+			<td class='head'>Size</td></tr>\n");
 
-         // print 'em
-         print("<h1>Localhost Directory</h1>");
-         print("<table cellspacing='0' class='table'>
-                 <tr>
-                   <td class='head'>Filename</td>
-                   <td class='head'>Type</td>
-                   <td class='head'>Size</td></tr>\n");
+				 // loops through the array of files and print them all
+		foreach($dirArray as $item) {
+			if ($item != '.' && $item != '..'){ // don't list hidden files
+				if (is_dir($item)) {
+					echo "<tr><td><a class='folder' href='$item'>$item/</a></td>"."<td> Folder </td>"."<td>".round(filesize($item)/1000,2)." Mb"."</td>"."</tr>\n";
+				}else{
+					echo "<tr><td><a class='file' href='$item'>$item</a></td>"."<td>".findexts($item)."</td>"."<td>".round(filesize($item)/1000,2)." Mb"."</td>"."</tr>\n";
+				}
+			}
+		}
+		echo "</table>\n";
+		?>
+		</div>
 
-         // loops through the array of files and print them all
-         for($index=0; $index < $indexCount; $index++) {
-               if (substr("$dirArray[$index]", 0, 1) != "."){ // don't list hidden files
-					if (findexts($dirArray[$index]) == strtoupper($dirArray[$index])) {
-						echo "<tr><td><a class='folder' href='$dirArray[$index]'>$dirArray[$index]/</a></td>"."<td> Folder </td>"."<td>".round(filesize($dirArray[$index])/1000,2)." Mb"."</td>"."</tr>\n";
-					}else{
-						echo "<tr><td><a class='file' href='$dirArray[$index]'>$dirArray[$index]</a></td>"."<td>".findexts($dirArray[$index])."</td>"."<td>".round(filesize($dirArray[$index])/1000,2)." Mb"."</td>"."</tr>\n";
-					}
-           	   }
-         }
-         echo "</table>\n";
-       ?>
-   </div>
-
-</body>
+	</body>
 </html>
