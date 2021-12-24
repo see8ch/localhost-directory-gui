99fa16283fa9bea0eccbfb353a55d21e1c3ffb07
Marco Galletti <51877341+m4rch1n0@users.noreply.github.com>
2021-12-24 01:20:19 +0100

    Add support to php8

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 		function findexts ($filename)
 		{
 			$filename = strtolower($filename) ;
-			$exts = split("[/\\.]", $filename) ;
+			$exts = explode("[/\\.]", $filename) ;
 			$n = count($exts)-1;
 			$exts = $exts[$n];
 			return strtoupper($exts);
